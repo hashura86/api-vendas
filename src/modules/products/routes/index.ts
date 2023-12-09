c0cdbb7371e6085c94421bab1a42b10b428f0ec2
João Ribeiro <joaoribeiro8699@gmail.com>
2023-12-09 10:14:04 -0300

    use celebrate validation in routes

--- src/modules/products/routes/index.ts
+++ src/modules/products/routes/index.ts
@@ -1,14 +1,58 @@
 import { Router } from 'express'
 import ProductController from '../controllers/ProductController'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 const productController = new ProductController()
 
 const productRoutes = Router()
 
-productRoutes.post('/', productController.create)
-productRoutes.get('/:id', productController.getOne)
 productRoutes.get('/', productController.index)
-productRoutes.put('/:id', productController.update)
-productRoutes.delete('/:id', productController.delete)
+
+productRoutes.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.getOne,
+)
+
+productRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.create,
+)
+
+productRoutes.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.update,
+)
+
+productRoutes.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+)
 
 export default productRoutes
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,13 +5,14 @@ import cors from 'cors'
 import routes from './routes'
 import middleware from '@shared/middlewares/middleware'
 import '@shared/typeorm'
-// import AppError from '@shared/exceptions/AppError'
+import { errors } from 'celebrate'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 app.use(middleware.errorHandler)
 
 app.listen(3000, () => {
