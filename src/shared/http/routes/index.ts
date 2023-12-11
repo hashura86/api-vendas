c57967d4d0cb434a81e7f984b640e3f621fb06bf
João Ribeiro <joaoribeiro8699@gmail.com>
2023-12-11 10:42:10 -0300

    User module

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import productRoutes from '@module/products/routes'
+import usersRouter from '@module/users/routes'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.use('/products', productRoutes)
+routes.use('/users', usersRouter)
 
 export default routes
